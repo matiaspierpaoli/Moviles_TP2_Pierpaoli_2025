59185c5b012dc1643bb45ac1af5baaae3420a935
Matias <matiaspierpaoli@gmail.com>
2025-11-19 16:25:17 -0300

    Added icons and adaptative background/foreground

--- Moviles_TP2_2025_Pierpaoli/Assets/Scripts/Core/Data/EconomyConfig.cs
+++ Moviles_TP2_2025_Pierpaoli/Assets/Scripts/Core/Data/EconomyConfig.cs
@@ -5,9 +5,7 @@ namespace Game.Core.Data
     [CreateAssetMenu(fileName = "EconomyConfig", menuName = "Game/Economy")]
     public class EconomyConfig : ScriptableObject
     {
-        public int rewardPerLevel = 25;
         public int rewardPerCoin = 1;
-        public int itemPrice = 50;
         public int hiddenLevelPrice = 5;
     }
 }
